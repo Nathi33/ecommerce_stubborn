5a0212fb0921d121fa8088f0c203f648978d6c5a
Nathi33 <nathi33.drnt@outlook.com>
2025-04-16 11:59:28 +0200

    add boutique

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller;
 
-use App\Entity\Sweatshirt;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\SweatshirtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'app_shop')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(SweatshirtRepository $sweatshirtRepository, Request $request): Response
     {
-        $sweatshirts = $em->getRepository(Sweatshirt::class)->findAll();
+        //$sweatshirts = $sweatshirtRepository->findAll();
+        $priceRange = $request->query->get('priceRange');
+        $sweatshirts = [];
+
+        if ($priceRange) {
+            [$min, $max] = explode('-', $priceRange);
+            $sweatshirts = $sweatshirtRepository->createQueryBuilder('s')
+                ->where('s.price >= :min')
+                ->andWhere('s.price <= :max')
+                ->setParameter('min', $min)
+                ->setParameter('max', $max)
+                ->getQuery()
+                ->getResult();
+        } else {
+            $sweatshirts = $sweatshirtRepository->findAll();
+        }
         return $this->render('shop/index.html.twig', [
             'sweatshirts' => $sweatshirts,
         ]);
